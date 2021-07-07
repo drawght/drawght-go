23e32b77fe768aae541f978bf5cc62f573a79c6f
Hallison Batista <email@hallison.dev.br>
2021-07-07 16:37:22 -0400

    Test improvements

--- drawght_test.go
+++ drawght_test.go
@@ -67,37 +67,23 @@ func TestParseKeys(t *testing.T) {
 	}
 }
 
-func TestParseQueries(t *testing.T) {
-	var dataset = getDataset()
+func TestParseQueryKeys(t *testing.T) {
 	var templatesWithQueries = map[string]string {
-		"# {title} written by {author.name}": "# Drawght is a very useful sketch written by Hallison Batista",
+		"# Written by {author.name}": "# Written by Hallison Batista",
+	}
+
+	testParse(drawght.ParseKeys, t, templatesWithQueries, getDataset())
+}
+
+func TestParseQueryItems(t *testing.T) {
+	var templatesWithQueries = map[string]string {
 		"- {references#1.name}": "- Mustache",
 		"- {references#2.name}": "- Handlebars",
 		"- {references#3.name}": "- {references#3.name}",
 		"- {tags}": "- Template\n- Draft",
 	}
 
-	templateLines := make([]string, len(templatesWithQueries))
-	expectedLines := make([]string, len(templatesWithQueries))
-
-	i := 0
-	for template, expected := range(templatesWithQueries) {
-		templateLines[i] = template
-		expectedLines[i] = expected
-		i++
-	}
-
-	template := strings.Join(templateLines[:], "\n\n")
-	expected := strings.Join(expectedLines[:], "\n\n")
-
-	result := drawght.ParseQueries(template, dataset)
-
-	if result != expected {
-		t.Errorf("Template not parsed")
-		t.Errorf("Template:\n%v\n", template)
-		t.Errorf("Expected:\n%v\n", expected)
-		t.Errorf("Result:\n%v\n", result)
-	}
+	testParse(drawght.ParseKeys, t, templatesWithQueries, getDataset())
 }
 
 func TestParse(t *testing.T) {
@@ -116,4 +102,28 @@ func getDataset() (dataset map[string]interface{}) {
 	}
 
 	return dataset
+}
+
+func testParse(parse func(string, map[string]interface{})(string), t *testing.T, templatesWithQueries map[string]string, dataset map[string]interface{}) {
+	templateLines := make([]string, len(templatesWithQueries))
+	expectedLines := make([]string, len(templatesWithQueries))
+
+	i := 0
+	for template, expected := range(templatesWithQueries) {
+		templateLines[i] = template
+		expectedLines[i] = expected
+		i++
+	}
+
+	template := strings.Join(templateLines[:], "\n\n")
+	expected := strings.Join(expectedLines[:], "\n\n")
+
+	result := parse(template, dataset)
+
+	if result != expected {
+		t.Errorf("Template not parsed")
+		t.Errorf("Template:\n%v\n", template)
+		t.Errorf("Expected:\n%v\n", expected)
+		t.Errorf("Result:\n%v\n", result)
+	}
 }
