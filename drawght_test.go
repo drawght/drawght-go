3706baaa2020b765363730d3687b63f006739729
Hallison Batista <email@hallison.dev.br>
2021-07-07 00:03:40 -0400

    Test improvements

--- drawght_test.go
+++ drawght_test.go
@@ -8,22 +8,22 @@ import (
 	"testing"
 )
 
+var dataset = map[string]interface{} {
+	"title": "This is a title",
+	"author": "Nathan Never",
+	"tags": []string {
+		"Template",
+		"Draft",
+	},
+}
+
+var templates = map[string]string {
+	"<h1>{title}</h1>": fmt.Sprintf("<h1>%s</h1>", dataset["title"]),
+	"# {title} written by {author}": fmt.Sprintf("# %s written by %s", dataset["title"], dataset["author"]),
+	"- {tags}": "",
+}
+
 func TestParseTemplate(t *testing.T) {
-	dataset := map[string]interface{} {
-		"title": "This is a title",
-		"author": "Nathan Never",
-		"tags": []string {
-			"Template",
-			"Draft",
-		},
-	}
-
-	templates := map[string]string {
-		"<h1>{title}</h1>": fmt.Sprintf("<h1>%s</h1>", dataset["title"]),
-		"# {title} written by {author}": fmt.Sprintf("# %s written by %s", dataset["title"], dataset["author"]),
-		"- {tags}": "",
-	}
-
 	tagsSlice := make([]string, len(dataset["tags"].([]string)))
 
 	for i, tag := range (dataset["tags"].([]string)) {
@@ -42,5 +42,5 @@ func TestParseTemplate(t *testing.T) {
 }
 
 func TestParse(t *testing.T) {
-	return
+	t.Skip("Not implemented, yet")
 }
